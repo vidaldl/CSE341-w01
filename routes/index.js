69604ca9b6d898cf3123a8519a066a053e908d2d
vidaldl <vidaldl@outlook.com>
2024-05-18 20:44:48 -0600

    added swagger

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 const routes = require('express').Router();
 
-// Controllers
-const nameController = require('../controllers/nameController');
 
-
-
-routes.get('/', nameController.enmanuel); 
-
+routes.use('/', require('./swagger'));
 routes.use('/contacts', require('./contacts'));
 
 
